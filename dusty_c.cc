36cbb67bc4531868ab1ab49bfa48fff945e74684
Adam L. Steinberg <steinberg_al@hammer.cs.mercer.edu>
2022-01-22 15:06:44 -0500

    Trying to link timing libs to C

---
+++ dusty_c.cc
@@ -0,0 +1,14 @@
+//  The ancient dusty deck code rewritten in C++
+//  Modified to use timing libs in 2016
+//  Modified to use standardized RNG in 2018
+
+//prototypes
+double walltime_();
+double cputime_();
+
+int main()
+{
+  double wall, cpu;
+  wall = walltime_();
+  cpu  = cputime_();
+}
